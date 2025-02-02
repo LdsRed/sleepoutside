4ce5d919040b17a9c22489f7ea196fc056bec5fb
JSalvatierrajkl <joeljsalvatierra@gmail.com>
2025-02-01 22:40:37 -0300

    Product Searching

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -9,4 +9,26 @@ const dataSource = new ExternalServices();
 
 const productList = new ProductList(category, dataSource, '.product-list');
 productList.init();
-
+// Adding searching event
+document.addEventListener('DOMContentLoaded', () => {
+    const searchForm = qs('#searchForm');
+    const searchInput = qs('#searchInput');
+  
+    if (searchForm) {
+      searchForm.addEventListener('submit', async (event) => {
+        event.preventDefault();
+        const query = searchInput.value.trim().toLowerCase();
+  
+        if (query) {
+          // Obtener productos y filtrarlos
+          const allProducts = await dataSource.getData(category);
+          const filteredProducts = allProducts.filter(product =>
+            product.NameWithoutBrand.toLowerCase().includes(query)
+          );
+  
+          // Renderizar los productos filtrados
+          productList.renderList(filteredProducts);
+        }
+      });
+    }
+  });
