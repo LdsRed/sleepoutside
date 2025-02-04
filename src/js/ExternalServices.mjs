b6c129174d7a773ca5454a7499cc2f34e18cc578
CenterValentine <david@doxy.me>
2025-02-03 21:04:46 -0700

    minor changes

--- src/js/ExternalServices.mjs
+++ src/js/ExternalServices.mjs
@@ -1,11 +1,15 @@
-const baseURL = 'http://wdd330-backend.onrender.com/checkout';
+// const baseURL = 'http://wdd330-backend.onrender.com/checkout';
+const baseURL = import.meta.env.VITE_SERVER_URL;
+
+
+
 
 function convertToJson(res) {
+  jsonResponse = res.json();
   if (res.ok) {
-    return res.json();
+    return jsonResponse;
   } else {
-    throw new Error("Bad Response");
-  }
+    throw {name: 'servicesError', message: jsonResponse};  }
 }
 
 export default class ExternalServices {
